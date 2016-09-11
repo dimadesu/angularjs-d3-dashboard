39d150331ad3c4904ffd826f44086aed67fb2658
Dmitry Antonov <antonov.dmitriy@gmail.com>
2016-09-11 09:50:50 +0900

    Moved data to JSON

--- app/services/processes.js
+++ app/services/processes.js
@@ -1,37 +1,16 @@
 'use strict';
 
 angular.module('app.services')
-.factory('processesService', function () {
-  var _processes = [
-    {
-      id: 1,
-      cpu: 73,
-      status: 'On',
-      instances: 4,
-      memory: 1,
-      upTime: '00:00:20',
-      restarts: 0
-    },
-    {
-      id: 2,
-      cpu: 50,
-      status: 'Off',
-      instances: 2,
-      memory: 50,
-      upTime: '00:03:00',
-      restarts: 3
-    },
-    {
-      id: 3,
-      cpu: 10,
-      status: 'On',
-      instances: 1,
-      memory: 9,
-      upTime: '04:00:00',
-      restarts: 0
-    }
-  ];
+.factory('processesService', ['$http', function ($http) {
+  var _processes = [];
   var _processIdsRendered = [];
+
+  $http.get('/services/processes.json').then(function(response) {
+      _processes = response.data;
+    }, function(response) {
+      console.log(response);
+    }
+  );
   
   function _getNotRenderedItem(processes, processIdsRendered) {
     var idsNotRendered = processes.map(function (item) {
@@ -68,4 +47,4 @@ angular.module('app.services')
   };
   
   return processesService;
-});
+}]);
